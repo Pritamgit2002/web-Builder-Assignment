dcfee365dc4a64c596e8c486a8468b3655b06aa4
Pritam Gain <pritamjob2021@gmail.com>
2024-02-29 03:19:28 +0530

    style:navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,7 +9,7 @@ const navLinks = [
 ];
 export default function Navbar() {
   return (
-    <div className="w-full flex items-center justify-center gap-8 bg-[#212731] mx-auto h-16 ">
+    <div className="w-full flex items-center justify-center gap-6 sm:gap-8 bg-[#212731] mx-auto h-16 ">
       <div className="w-64 h-9 bg-white flex items-center justify-center gap-1 border-[1px] rounded-lg pl-2 ">
         <div className=" text-xl sm:text-2xl ">
           <PiMagnifyingGlassLight />
@@ -20,7 +20,7 @@ export default function Navbar() {
           className=" w-full h-full flex items-center justify-center rounded-r-lg peer-disabled focus:outline-none "
         />
       </div>
-      <div className=" flex items-center justify-center gap-8 ">
+      <div className=" flex items-center justify-center gap-6 sm:gap-8 ">
         {navLinks.map((item, i) => (
           <Link href={item.link} key={i}>
             <div className=" text-[#D1D6DA] hover:text-white ">
